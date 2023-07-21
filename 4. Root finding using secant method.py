551b8b0878298141fa94717516995411230de015
Ashlin-V-Thomas <vthomasashlin@gmail.com>
2023-07-21 20:03:59 +0530

    Updated 4. Root finding using secant method.py

--- 4. Root finding using secant method.py
+++ 4. Root finding using secant method.py
@@ -1,19 +1,32 @@
-#Program to find root of a function using secant method.
+#Program to find the root of a function using secant method.
 
 def f(x): #function whose roots have to be found out.
-    return x**3 - 7*x**2 +14*x -8
+    return x**2 -5*x +6
 
 
 def find_root(f,x_1,x_2, tolerance):
-    #The function returns only one root of f. For other roots, try chaanging x_1 and x_2.
+    #The function returns only one root of f. For other roots, try changing x_1 and x_2.
     x_n = x_1
     x_n1 = x_2
+    if f(x_n)==f(x_n1):
+        print(" Try a different choice of x_1 and x_2.")
+        return None
     x_n2 = x_n1 - ( f(x_n1)*(x_n1 - x_n) / (f(x_n1) - f(x_n)) )
     while abs(x_n2 - x_n1) > tolerance:
-        #print(x_n2)
         x_n = x_n1
         x_n1 = x_n2
-        x_n2 = x_n1 - ( f(x_n1)*(x_n1 - x_n) / (f(x_n1) - f(x_n)) )
-    return x_n2
+        if f(x_n)==f(x_n1):
+            print(" Try a different choice of x_1 and x_2.")
+            return None
+        else:
+            x_n2 = x_n1 - ( f(x_n1)*(x_n1 - x_n) / (f(x_n1) - f(x_n)) )
+    if int(f(x_n2)*100) == 0:
+        return x_n2
+    else:
+        print("Try reducing the tolerance.")
+        return
 
-print(find_root(f,4,10,10**-3))
+root = find_root(f,0,10,10**-3)
+if root is not None:
+    print(root , "is the root of the function.")
+
